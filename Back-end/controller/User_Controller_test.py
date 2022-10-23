ef24830f4d99fa69e9f91978939e47c95e7a58a9
ahroncich7 <alehroncich@gmail.com>
2022-10-23 19:29:34 -0300

    Crea pruebas y BD de pruebas

--- Back-end/controller/User_Controller_test.py
+++ Back-end/controller/User_Controller_test.py
@@ -1,3 +1,4 @@
+
 from User_Controller import User_Controller
 from model.User import User
 
@@ -9,12 +10,13 @@ if __name__ == "__main__":
 
 
     # Crear usuarios falsos para poblar la lista
-    user1 = User("pete_the_great@gmail.com", "sqllover123", False, "Pete")
+    user1 = User_Controller.register("pete_the_great@gmail.com", "sqllover123", "Pete")
     user1.portfolio_id = 1
-    user2 = User("jdoe89@hotmail.com", "27051989", False, "John Doe")
+    user2 = User_Controller.register("jdoe89@hotmail.com", "27051989", "John Doe")
     user2.portfolio_id = 2
-    user3 = User("casandrawilliams@gmail.com", "cswl897", False, "Caswill")
+    user3 = User_Controller.register("casandrawilliams@gmail.com", "cswl897", "Caswill")
     user3.portfolio_id = 3
+
 
  # Caso de uso 1: Usuario erróneo
 
@@ -37,12 +39,14 @@ if __name__ == "__main__":
         print(
             "#"*5, f"Efectivamente dio error: {e}", "#" * 5, sep="\n", end="\n")
 
-    # Caso de uso3: Usuario y contraseña correctos
+    # Caso de uso 3: Usuario y contraseña correctos
     print("Caso de uso 3: Usuario y contraseña correctos. Se espera loguearse exitosamente")
 
     try:
-        test_user = User_Controller.login("jdoe89@hotmail.com", "27051989")
+        test_user = User_Controller.login("casandrawilliams@gmail.com", "cswl897")
         print(
             "#"*5, f"Usuario logueado: {test_user}", "#" * 5, sep="\n", end="\n")
     except Exception as e:
         print("#"*5, f"Fallo comprobación!!! {e}", "#" * 5, sep="\n", end="\n")
+
+   
