0ff638f1fa16b2bb4a3fec4502aef0be2fb96e02
Malvina <malvichrist@gmail.com>
2022-11-08 18:55:11 -0300

    Creacion de DB, tablas e ingreso de registros

---
+++ Back-end/model/DB/portfReg.sql
@@ -0,0 +1,37 @@
+/* Registros de Cartera */
+
+INSERT INTO Cartera
+SET idCartera = "",
+	id_Usuario = 1;
+    
+INSERT INTO Cartera
+SET idCartera = "",
+	id_Usuario = 6;
+    
+INSERT INTO Cartera
+SET idCartera = "",
+	id_Usuario = 3;
+    
+INSERT INTO Cartera
+SET idCartera = "",
+	id_Usuario = 2;
+    
+INSERT INTO Cartera
+SET idCartera = "",
+	id_Usuario = 4;
+
+INSERT INTO Cartera
+SET idCartera = "",
+	id_Usuario = 9;
+    
+INSERT INTO Cartera
+SET idCartera = "",
+	id_Usuario = 7;
+    
+INSERT INTO Cartera
+SET idCartera = "",
+	id_Usuario = 8;
+
+INSERT INTO Cartera
+SET idCartera = "",
+	id_Usuario = 5;
