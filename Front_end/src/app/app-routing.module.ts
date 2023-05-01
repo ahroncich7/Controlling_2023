489ef73e20f9e07396e6ffffc74a506f3b07be28
ahroncich7 <alehroncich@gmail.com>
2023-05-01 15:39:15 -0300

    FEAT: Define el routing

--- Front_end/src/app/app-routing.module.ts
+++ Front_end/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarteraComponent } from './pages/cartera/cartera.component';
+import { PerfilUsuarioComponent } from './pages/perfil-usuario/perfil-usuario.component';
+import { RegistroComponent } from './pages/registro/registro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: "cartera", component: CarteraComponent},
+  {path: "perfil_usuario", component: PerfilUsuarioComponent},
+  {path: "registro", component: RegistroComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
